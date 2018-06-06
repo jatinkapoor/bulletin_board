377388122afd19a43beff9c732c08eeae1ba5dc6
Jatin.Kapoor <jatin.kapoor@target.com>
2018-06-06 14:48:48 -0500

    styled version 1.0

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -16,13 +16,5 @@ $(document).ready(() => {
   //   const pass = $('#login-pass').trim().val();
   // });
 
-  $('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this)
-    modal.find('.modal-title').text('New message to ' + recipient)
-    modal.find('.modal-body input').val(recipient)
-  });
+  
 });
